d1f4e37becb793c11651856bd8011d6a20bf226f
JoojBoogeArc <Faulkner-T2@ulster.ac.uk>
2025-11-03 13:39:59 +0000

    Encapsulation

--- library/Book.cs
+++ library/Book.cs
@@ -2,17 +2,35 @@
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title;
+        private string author;
+        private string isbn;
 
-        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        public string Title
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            get { return title; }
+            set { title = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
         }
 
+        // Constructor to add a new book
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
+        }
+
+        // Method to display information about a book
         public void DisplayInfo()
         {
             Console.WriteLine($"Book title: {Title}");
